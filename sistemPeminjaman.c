3cad343078ad2786be7711eb355c13b822844889
annisa.riki <fadhilaah@mhs.usk.ac.id>
2025-11-19 19:41:58 +0700

    Update sistemPeminjaman.c

--- sistemPeminjaman.c
+++ sistemPeminjaman.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void menuUser();
+void menuAdmin();
+void tambahAlat();
+void hapusAlat();
+void editAlat();
+void pinjamAlat();
+void kembalikanAlat();
+void lihatAlat();
+void listAlat();
+
+typedef struct {
+    unsigned int id;
+    char nama[50];
+    char merek[50];
+    char model[50];
+    unsigned int tahun;
+    unsigned int jumlah;
+    unsigned int tersedia;
+}
