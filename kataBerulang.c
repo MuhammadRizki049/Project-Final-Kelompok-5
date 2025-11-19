b266485d4bf24eb9fc22e498bbcd55b97460d091
asysyifaa <asysyifaa@mhs.usk.ac.id>
2025-11-19 19:06:18 +0700

    Update kataBerulang.c

--- kataBerulang.c
+++ kataBerulang.c
@@ -1,1 +1,16 @@
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <string.h>  
+#include <ctype.h> 
+
+#define MAX_LINE_LENGTH 1024  
+#define MAX_WORD_LENGTH 100  
+#define MAX_UNIQUE_WORDS 5000 
+
+char unique_words[MAX_UNIQUE_WORDS][MAX_WORD_LENGTH];
+int unique_word_count = 0;
+
+int is_word_char(char c) {
+    return isalpha(c) || c == '\'';
+}
 
