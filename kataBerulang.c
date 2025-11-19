b02643e729a7f861ecb574b28a4546650f010e5d
asysyifaa <asysyifaa@mhs.usk.ac.id>
2025-11-19 19:07:54 +0700

    Update kataBerulang.c

--- kataBerulang.c
+++ kataBerulang.c
@@ -14,3 +14,17 @@ int is_word_char(char c) {
     return isalpha(c) || c == '\'';
 }
 
+void add_unique_word(char* word) {
+    for (int i = 0; i < unique_word_count; i++) {
+        if (strcmp(unique_words[i], word) == 0) {
+            return; 
+        }
+    }
+
+    if (unique_word_count < MAX_UNIQUE_WORDS) {
+        strcpy(unique_words[unique_word_count], word);
+        unique_word_count++;
+    } else {
+        fprintf(stderr, "Peringatan: Jumlah kata unik maksimum (%d) telah tercapai.\n", MAX_UNIQUE_WORDS);
+    }
+}
